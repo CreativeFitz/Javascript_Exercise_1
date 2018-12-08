77074988fc1f6547d414adc79eaa04f4277cc4db
Newforce <thecreativefitz@gmail.com>
2018-12-08 11:52:23 -0500

    completed first Javascript exercise file

---
+++ script.js
@@ -0,0 +1,35 @@
+// alert("Javascript works!");
+console.log ("Hello, World!");
+// Exerice 1: All About Me
+var firstName = "Bobby";
+var lastName = "Fitzpatrick";
+var age = "28";
+var myName = "Bobby Fitzpatrick";
+myName = "TheFitz";
+var favoriteFood = "Spaghetti";
+var likesTravel = true;
+
+// Exercise 2: Favorite Movies
+var favoriteMovieArray = ["Interstellar", "The Matrix", "Inception"];
+var firstMovie = favoriteMovieArray[0];
+var secondMovie = favoriteMovieArray[1];
+var thirdMovie = favoriteMovieArray[2];
+console.log(favoriteMovieArray.length);
+favoriteMovieArray.push("Aladdin");
+favoriteMovieArray[3] = "Treasure Planet";
+
+// Exercise 3: Student Grades
+var WilliamGrades = [62, 97, 99, 85, 73, 97];
+var averageGrade =(WilliamGrades[0] + WilliamGrades[1] + WilliamGrades[2] + WilliamGrades[3] + WilliamGrades[4] + WilliamGrades[5])/6;
+
+// Exercise 4: Movie Star
+var moviestarFirstName = "Keanu";
+var moviestarLastName = "Reeves";
+var moviestarAge= "54";
+var moviestarFavoriteFood= "Steak";
+moviestarFullname = `${moviestarFirstName} ${moviestarLastName}`;
+var shortBio = `${moviestarFullname} is ${moviestarAge}. His favorite food is ${moviestarFavoriteFood}`;
+
+
+
+
